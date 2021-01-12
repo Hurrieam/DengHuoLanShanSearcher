89986e5967053c133e609731f4ff27ed10d52262
Hurrieam <77335318+Hurrieam@users.noreply.github.com>
2021-01-13 02:00:37 +0800

    Add files via upload

---
+++ js/LanguageToggle.js
@@ -0,0 +1,17 @@
+function simplifiedChinese() {
+    document.getElementById("title").innerHTML = "灯火阑珊飓风搜索";
+    document.getElementById("search").innerHTML = "搜索";
+    document.getElementById("multiLanguage").innerHTML = "多语种";
+}
+
+function English() {
+    document.getElementById("title").innerHTML = "Invincible Search";
+    document.getElementById("search").innerHTML = "Go";
+    document.getElementById("multiLanguage").innerHTML = "Multilanguage";
+}
+
+function Nihongo() {
+    document.getElementById("title").innerHTML = "無敵な検索エンジン";
+    document.getElementById("search").innerHTML = "検索する";
+    document.getElementById("multiLanguage").innerHTML = "言語";
+}
---
+++ js/search.js
@@ -0,0 +1,3 @@
+function search() {
+    window.location.href = "https://www.baidu.com/s?ie=UTF-8&wd=" + document.getElementById("content").value;
+}
